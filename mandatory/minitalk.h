5f6a703131dc24b0736ea6c069f85c82e5e0d28c
Nuno Louro <nlouro@student.42heilbronn.de>
2021-12-03 12:18:29 +0100

    added individual header files for each function used

--- mandatory/minitalk.h
+++ mandatory/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: nlouro <nlouro@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/17 21:45:34 by nlouro            #+#    #+#             */
-/*   Updated: 2021/11/30 15:46:16 by nlouro           ###   ########.fr       */
+/*   Updated: 2021/12/03 12:11:11 by nlouro           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 # include <signal.h>
 # include <stdlib.h> //malloc
 # include <stdio.h> //printf
-# include "ft_isdigit.c"
-# include "ft_atoi.c"
-# include "ft_itoa.c"
+# include "ft_isdigit.h"
+# include "ft_atoi.h"
+# include "ft_itoa.h"
 
 #endif
